8803cd32707d4b863e38204c89b3ba3685fb145f
cyuu0020 <cyuu0020@student.monash.edu>
2019-09-15 13:38:25 +1000

    inital

---
+++ W7_Assignment/W7_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W7_Assignment.MvcApplication" Language="C#" %>
---
+++ W7_Assignment/W7_Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W7_Assignment.Startup))]
+namespace W7_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
